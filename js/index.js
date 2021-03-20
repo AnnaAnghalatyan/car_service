520ccf53fc15a2d8ae769a3b936510f58705f0af
Anna Anghalatyan <annaa@local.express>
2021-03-20 20:06:07 +0400

    commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
-$('.content-clickable').on('click', function () {
-    $(this).toggleClass('active');
-    // let active_blocks = $('.content-clickable.active');
-    // active_blocks.removeClass('active');
+$(document).ready(function () {
+    $(window).scrollTop() > 20 ? $('.menu').addClass('scrolled') : $('.menu').removeClass('scrolled')
 
-})
+    $('.content-clickable').on('click', function () {
+        let active = $(this).hasClass('active');
+        console.log(active)
 
-$('.menu__sub-toggler').on('click', function () {
-    $(this).closest('.menu__item').toggleClass('open')
+        $('.content-clickable').removeClass('active');
+        active ? $(this).removeClass('active') : $(this).addClass('active');
+    })
+
+    $('.menu-toggler, .menu__overlay, .menu__toggler-mobile').on('click', function (e) {
+        e.stopPropagation()
+        ToggleMenu()
+    })
+
+    function ToggleMenu() {
+        $(document.body).toggleClass('overflow-hidden');
+        $('.menu__list').toggleClass('open');
+        $('.menu__overlay').toggleClass('show')
+    }
+
+    $(".header__carousel").owlCarousel({
+        items: 1,
+        center: true,
+        loop: true,
+        dots: false,
+        nav: true,
+        animateOut: 'fadeOut',
+        autoplay: true,
+        autoplayTimeout: 3000,
+    });
+
+    $( window ).scroll(function(e) {
+        $(window).scrollTop() > 40 ? $('.menu').addClass('scrolled') : $('.menu').removeClass('scrolled')
+    });
 })
